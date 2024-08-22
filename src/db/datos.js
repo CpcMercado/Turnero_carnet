45136a1c5100a630505974dcebe85c5440939ec5
nestorabel.pierobon <nestorabel.pierobon@gmail.com>
2024-08-22 09:59:15 -0300

    variable de entorno

--- src/db/datos.js
+++ src/db/datos.js
@@ -1,14 +1,13 @@
-import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFJmuU5bBQzpNqBFWmtOegTWMM99enV4A",
-    authDomain: "cpcmercado-21580.firebaseapp.com",
-    projectId: "cpcmercado-21580",
-    storageBucket: "cpcmercado-21580.appspot.com",
-    messagingSenderId: "717110775909",
-    appId: "1:717110775909:web:3761e1c96c19a95238f68c"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
